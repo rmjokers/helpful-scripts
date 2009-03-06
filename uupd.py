1085f5419bea93df0ca06cdbfffb6fa202c3ed0d
Jokerst <jokerrm1@jokerrm1-ml1.jhuapl.edu>
2009-03-06 11:04:59 -0500

    Renamed dependency script and updated to make more efficient.

--- uupd.py
+++ uupd.py
@@ -1,3 +1,9 @@
+# Resolve Ubuntu universe package dependencies.
+#
+# Given an Ubuntu package, identify all dependencies from the Universe
+# repository and get a list of HREFs for those packages. This allows us to
+# download the packages and install them offline more easily.
+
 import re
 import sys
 import urllib
@@ -5,7 +11,7 @@ import time # for sleep
 import random # for sleep time
 
 if (len(sys.argv) <= 3):
-  print "Usage: python extract-hrefs2.py ghex hardy amd64"
+  print "Usage: python uups.py ghex hardy amd64"
   exit(1)
 
 # Command line args
@@ -13,8 +19,6 @@ package = sys.argv[1]
 distro = sys.argv[2] # hardy
 arch = sys.argv[3] # amd64 / i386
 
-start = False
-end = False
 base_url = "http://packages.ubuntu.com"
 # Relative name in the format the website will return to us
 root_package_name = "/" + distro + "/" + package
@@ -25,6 +29,7 @@ full_href_re = re.compile("a href=\"(http:\S+universe\S+)\"")
 distro_re = re.compile(distro)
 dep_re = re.compile("<ul class=\"uldep\">")
 ndep_re = re.compile("</ul>")
+universe_re = re.compile(">universe<")
 
 # List of all packages we have examined
 all_packages = list()
@@ -34,8 +39,13 @@ all_hrefs = list()
 packages_to_load = list()
 packages_to_load.append(root_package_name)
 
+start = False
+end = False
+universe = False
+
 # Keep going until there are no packages left to load
 while len(packages_to_load) > 0:
+
   # Remove first element of to-load list
   element = packages_to_load.pop(0)
   # Add this element to the list of all packages
@@ -45,10 +55,16 @@ while len(packages_to_load) > 0:
   file = urllib.urlopen(base_url + element)
 
   for line in file:
+
+    unv = universe_re.search(line)
     dep = dep_re.search(line)
 
+    # First, look for the universe marker. We only care about packages from universe for now
+    if unv:
+      universe = True
+
     # Wait until we find marker for start of dependencies section
-    if dep:
+    if universe and dep:
       start = True
 
     if start:
@@ -64,35 +80,41 @@ while len(packages_to_load) > 0:
 	if d and not entry in all_packages and not entry in packages_to_load:
 	  # Add any packages we have not already seen to the load list
 	  packages_to_load.append(entry)
-	  print "Adding " + entry + " to load list length now " + str(len(packages_to_load))
-	  time.sleep(1)
-
-	  # URL for the download page for the package
-	  package_name = entry.split('/')[2]
-	  download_url = base_url + "/" + distro + "/" + arch + "/" + package_name + "/download"
-	  print "Downloading URL: " + download_url
-
-	  download = urllib.urlopen(download_url)
-
-	  for d in download:
-	    href = full_href_re.search(d)
-	    if href:
-	      entry = href.group(1)
-	      # Need to escape package name in case it has special characters
-	      # that would break our regular expression
-	      temp_re = re.compile(re.escape(package_name))
-	      if temp_re.search(entry):
-		print "HREF: " + entry
-		all_hrefs.append(entry)
-		break
-
-	  download.close() 
+	  print "\tAdding " + entry + " to dependency list length now " + str(len(packages_to_load))
 
       if ndep:
 	end = True
 	start = False
 
   file.close()
+
+
+  # Now that we are finished processing the package description, 
+  # URL for the download page for the package
+  package_name = element.split('/')[2]
+  download_url = base_url + "/" + distro + "/" + arch + "/" + package_name + "/download"
+  print "\tExamining download URL: " + download_url
+
+  download = urllib.urlopen(download_url)
+
+  for d in download:
+    href = full_href_re.search(d)
+    if href:
+      entry = href.group(1)
+      # Need to escape package name in case it has special characters
+      # that would break our regular expression
+      temp_re = re.compile(re.escape(package_name))
+      if temp_re.search(entry):
+	print "\tHREF: " + entry
+	all_hrefs.append(entry)
+	break
+
+  download.close() 
+
+  universe = False
+  start = False
+  end = False
+  
   time.sleep(random.randrange(1,10))
 
 print "Full dependency list:"
